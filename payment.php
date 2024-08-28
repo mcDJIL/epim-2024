07fbbb4086f17de7dd7c0854e5aa591e610bffb9
mcDJIL <mdjauharil29@gmail.com>
2024-08-29 00:20:51 +0700

    create landing page create detail ticket page create payment page create print pdf page

---
+++ payment.php
@@ -0,0 +1,403 @@
+<?php
+
+$id_ticket = $_GET['id-ticket'] ?? '';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Seeds</title>
+    <link rel="stylesheet" href="./payment.css">
+    <link rel="shortcut icon" href="./assets/images/favicon.png" type="image/x-icon">
+</head>
+<body>
+<nav>
+        <a href="/">
+            <div class="nav-title">
+                <img src="./assets/images/logo.png" alt="">
+            </div>
+        </a>
+
+            <div class="nav-item-wrapper">
+                <div class="nav-link nav-item link-events" data-link="events-section">
+                    Events
+                </div>
+                <div class="nav-link nav-item link-personnel" data-link="personnel-section">
+                    Personnel
+                </div>
+                <div class="nav-link nav-item link-seats" data-link="seats-section">
+                    Seats
+                </div>
+                <div class="nav-link nav-item link-schedule" data-link="schedule-section">
+                    Schedule
+                </div>
+                <div class="nav-link nav-item link-weather" data-link="weather-section">
+                    Weather
+                </div>
+            </div>
+
+            <div class="nav-button">
+                <button class="btn-tickets">Get Ticket</button>
+                <a href="./pages/login.php">Login</a>
+            </div>
+
+            <div class="nav-item-mobile">
+                <div class="left-nav">
+                    <img src="./assets/images/logo.png" alt="">
+                </div>
+
+                <div class="right-nav">
+                    <div class="nav-button-mobile">
+                        <button class="btn-tickets">Get Ticket</button>
+                    </div>
+
+                    <div class="hamburger-container">
+                        <div class="hamburger-wrapper">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="nav-item-wrapper-mobile">
+                <div class="top-mobile">
+                    <div class="left-mobile">
+                        <img src="./assets/images/logo.png" alt="">
+                    </div>
+
+                    <div class="right-mobile">
+                        <img class="btn-close" src="./assets/svg/x_symbol.svg" alt="">
+                    </div>
+                </div>
+
+                <div class="bottom-mobile">
+                <div class="nav-link nav-item link-events" data-link="events-section">
+                    Events
+                </div>
+                <div class="nav-link nav-item link-personnel" data-link="personnel-section">
+                    Personnel
+                </div>
+                <div class="nav-link nav-item link-seats" data-link="seats-section">
+                    Seats
+                </div>
+                <div class="nav-link nav-item link-schedule" data-link="schedule-section">
+                    Schedule
+                </div>
+                <div class="nav-link nav-item link-weather" data-link="weather-section">
+                    Weather
+                </div>
+
+                <a href="./pages/login.php">Login</a>
+                </div>
+            </div>
+    </nav>
+
+    <div class="bg">
+        <section id="payment-section">
+            <div class="payment-container">
+                <div class="payment-wrapper">
+                    <div class="payment-countdown">
+                        <div class="payment-title">
+                            <h1>Complete Payment in</h1>
+                        </div>
+
+                        <div class="countdown">
+                    <div class="hours">
+                        <p id="hours">00</p>
+                    </div>
+
+                    <div class="colon">
+                        <p>:</p>
+                    </div>
+                    <div class="minutes">
+                        <p id="minutes">00</p>
+                    </div>
+
+                    <div class="colon">
+                        <p>:</p>
+                    </div>
+                    <div class="seconds">
+                        <p id="seconds">00</p>
+                    </div>
+                </div>
+                    </div>
+
+                    <div class="payment-instruction">
+                        <div class="left-instruction">
+                            <div class="instruction-title">
+                                <h1>Payment Instructions</h1>
+                            </div>
+
+                            <div class="instruction">
+                                <p>Thank you for placing your order. Please select one of the following banks to make payment:
+                                    </p>
+
+                                    <ol>
+                                        <li>Bank Transfer</li>
+                                        <ul>
+                                            <li>BCA: Account Number: [1234 5678 9123], in the Name: The Seeds team &  Bank Code: 014
+                                            </li>
+                                            <li>CIMB Niaga: Account Number: [1234 5678 9123], in the Name: The Seeds team & Bank Code: 014
+                                            </li>
+                                            <li>Mandiri: Account Number: [1234 5678 9123], in the Name: The Seeds team & Bank Code: 014
+                                            </li>
+                                            <li>BRI: Account Number: [1234 5678 9123], in the Name: The Seeds team & Bank Code: 014
+                                            </li>
+                                            <li>BNI: Account Number: [1234 5678 9123], in the Name: The Seeds team & Bank Code: 014
+                                            </li>
+                                        </ul>
+
+                                        <li>Make a Transfer</li>
+                                        <ul>
+                                            <li>Enter the account number and nominal according to your bill.
+                                            </li>
+                                            <li>Confirm and complete the transaction.
+                                            </li>
+                                        </ul>
+
+                                        <li>Save Proof of Payment</li>
+                                        <ul>
+                                            <li>Save proof of transfer, then we will send your e-ticket pdf via email.
+                                            </li>
+                                        </ul>
+                                    </ol>
+                            </div>
+                        </div>
+                        <div class="right-instruction">
+                                <div class="instruction-title">
+                                    <h1>Payment Details</h1>
+                                </div>
+
+                                <div class="instruction">
+                                <div class="instruction-top">
+                                    <p>ID Ticket</p>
+                                    <p><?= $id_ticket ?></p>
+                                </div>
+
+                                <div class="instruction-bottom">
+                                    <table>
+                                        <tr>
+                                            <td>Order date </td>
+                                            <td>: 02-08-2024 | 12:23</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Ticket Category </td>
+                                            <td>: VIP</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Quantity </td>
+                                            <td>: 1</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Sub Total</td>
+                                            <td>: RP 200,000</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Service Fee </td>
+                                            <td>: RP 2,500</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Total Payment </td>
+                                            <td>: RP 202,500</td>
+                                        </tr>
+                                    </table>
+                                </div>
+                                </div>
+
+                                <div class="instruction-upload">
+                                    <div class="instruction-title">
+                                        <h1>Upload Your Proof of Payment</h1>
+                                    </div>
+    
+                                    <div class="instruction-button">
+                                        <button class="btn-proof">Upload Proof of Payment</button>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="payment-footer">
+                            <p>If there is an error, immediately contact your bank or our customer support.
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    </div>
+
+    <footer>
+        <div class="footer-container">
+        <div class="nav-item-wrapper">
+                <div class="nav-link nav-item link-events" data-link="events-section">
+                    Events
+                </div>
+                <div class="nav-link nav-item link-personnel" data-link="personnel-section">
+                    Personnel
+                </div>
+                <div class="nav-link nav-item link-seats" data-link="seats-section">
+                    Seats
+                </div>
+
+                <div class="footer-logo">
+                    <a href="/">
+                        <img src="./assets/images/logo.png" alt="">
+                    </a>
+                </div>
+
+                <div class="nav-link nav-item link-tickets" data-link="tickets-section">
+                    Tickets
+                </div>
+                <div class="nav-link nav-item link-schedule" data-link="schedule-section">
+                    Schedule
+                </div>
+                <div class="nav-link nav-item link-weather" data-link="weather-section">
+                    Weather
+                </div>
+            </div>
+
+            <div class="footer-mobile">
+                <div class="footer-logo">
+                    <a href="/">
+                        <img src="./assets/images/logo.png" alt="">
+                    </a>
+                </div>
+
+                <div class="footer-link">
+                <div class="nav-link nav-item link-events" data-link="events-section">
+                    Events
+                </div>
+                <div class="nav-link nav-item link-personnel" data-link="personnel-section">
+                    Personnel
+                </div>
+                <div class="nav-link nav-item link-seats" data-link="seats-section">
+                    Seats
+                </div>
+                <div class="nav-link nav-item link-tickets" data-link="tickets-section">
+                    Tickets
+                </div>
+                <div class="nav-link nav-item link-schedule" data-link="schedule-section">
+                    Schedule
+                </div>
+                <div class="nav-link nav-item link-weather" data-link="weather-section">
+                    Weather
+                </div>
+                </div>
+            </div>
+
+            <div class="footer-bottom">
+                <p>&copy; Copyright by The Seeds 2024</p>
+            </div>
+        </div>
+    </footer>
+
+    <div class="overlay"></div>
+
+    <div class="modal modal-upload">
+        <div class="modal-header">
+            <h1>Proof of Payment</h1>
+            </div>
+
+        <div class="modal-body">
+            <img src="./assets/svg/image.svg" alt="">
+            <input type="file" name="image" id="image">
+        </div>
+
+        <div class="modal-footer">
+            <button class="btn-upload">Upload</button>
+        </div>
+    </div>
+    <div class="modal modal-refresh">
+        <div class="modal-header">
+            <h1>Successfully Uploaded!</h1>
+            </div>
+
+        <div class="modal-body">
+            <p>Thank you, Khairunnisa! Your proof of payment has been uploaded successfully.
+<br><br>
+Please be patient. Admin will confirm your proof of purchase within 3-5 minutes. Refresh the website regularly for more information.
+<br><br>
+See you at the concert, and let's have fun together! 🎶✨</p>
+        </div>
+
+        <div class="modal-footer">
+            <button class="btn-refresh">Refresh</button>
+        </div>
+    </div>
+    <div class="modal modal-pay-failed">
+        <div class="modal-header">
+            <h1>⚠️ Payment Confirmation Failed</h1>
+        </div>
+        <div class="modal-body">
+            <p>Sorry, Khairunnisa! Your payment could not be confirmed by our admin.
+<br><br>
+Please double check your receipt and ensure all details are correct. If you believe that this is an error, contact our support team for assistance.
+<br><br>
+We are here to help you get The Seeds concert tickets. Let's solve this problem! 🎶✨</p>
+        </div>
+
+        <div class="modal-footer">
+            <button class="btn-contact">Contact Admin</button>
+        </div>
+    </div>
+    <div class="modal modal-pay-success">
+        <div class="modal-header">
+            <h1>🎉 Payment Confirmed Successfully</h1>
+            </div>
+        <div class="modal-body">
+            <p>Thank you, Khairunnisa! Your payment has been successfully confirmed by our admin.
+<br><br>
+Your ticket for the The Seeds concert is on its way! Please check your email, including the spam or promotions folder, to find your ticket.
+<br><br>
+We can't wait to see you at the concert! Let's make it a night to remember! 🎶✨</p>
+        </div>
+
+        <div class="modal-footer">
+            <button class="btn-download">Download Ticket</button>
+        </div>
+    </div>
+
+    <script src="./payment.js"></script>
+    <script>
+        const btnProof = document.querySelector('.btn-proof');
+        const btnUpload = document.querySelector('.btn-upload');
+        const btnRefresh = document.querySelector('.btn-refresh');
+        const btnContact = document.querySelector('.btn-contact');
+        const btnDownload = document.querySelector('.btn-download');
+        const modalUpload = document.querySelector('.modal-upload');
+        const modalRefresh = document.querySelector('.modal-refresh');
+        const modalPayFailed = document.querySelector('.modal-pay-failed');
+        const modalPaySuccess = document.querySelector('.modal-pay-success');
+        const overlay = document.querySelector('.overlay');
+
+        btnProof.addEventListener('click', () => {
+            modalUpload.style.display = 'flex';
+            overlay.style.display = 'block';
+        })
+        btnUpload.addEventListener('click', () => {
+            modalUpload.style.display = 'none';
+            modalRefresh.style.display = 'flex';
+        })
+        btnRefresh.addEventListener('click', () => {
+            modalRefresh.style.display = 'none';
+            modalPayFailed.style.display = 'flex';
+        })
+        btnContact.addEventListener('click', () => {
+            modalPayFailed.style.display = 'none';
+            modalPaySuccess.style.display = 'flex';
+        })
+        btnDownload.addEventListener('click', () => {
+            modalPaySuccess.style.display = 'none';
+            overlay.style.display = 'none';
+
+            window.location.href = './pdf.php?id-ticket=<?= $id_ticket ?>';
+        })
+    </script>
+</body>
+</html>
---
+++ pdf.php
@@ -0,0 +1,326 @@
+<?php
+
+$id_ticket = $_GET['id-ticket'] ?? '';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PDF TICKET <?= $id_ticket ?></title>
+    <style>
+        @media print {
+            body {
+                -webkit-print-color-adjust: exact;
+            }
+        }
+
+        @font-face {
+    font-family: "Poppins";
+    src: url('./assets/fonts/Poppins-Regular.ttf');
+}
+@font-face {
+    font-family: "Poppins-Medium";
+    src: url('./assets/fonts/Poppins-Medium.ttf');
+}
+@font-face {
+    font-family: "Poppins-SemiBold";
+    src: url('./assets/fonts/Poppins-SemiBold.ttf');
+}
+@font-face {
+    font-family: "Poppins-Bold";
+    src: url('./assets/fonts/Poppins-Bold.ttf');
+}
+@font-face {
+    font-family: "NewRocker";
+    src: url('./assets/fonts/NewRocker-Regular.ttf');
+}
+
+.loader {
+    width: 100%;
+    height: 100vh;
+    background-color: var(--dark);
+    position: fixed;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    z-index: 110;
+    animation: loaderScreen 0.8s 2.2s forwards;
+}
+
+.loader-image {
+    width: 400px;
+    transform: translateX(-40px);
+    opacity: 0;
+    animation: loaderImage 0.3s 1.7s forwards;
+}
+
+.loader-text {
+    display: flex;
+    letter-spacing: 4px;
+    font-size: 40px;
+    color: var(--white);
+}
+
+.loader-text span {
+    font-family: "Poppins-Bold";
+    transform: translateY(40px);
+    opacity: 0;
+    animation: loaderText 0.3s forwards;
+}
+
+@keyframes loaderScreen {
+    to {
+        bottom: 100%;
+        opacity: 0;
+    }
+}
+
+@keyframes loaderImage {
+    100% {
+        opacity: 1;
+        transform: translateX(0px);
+    }
+}
+
+@keyframes loaderText {
+    0% {
+        opacity: 0;
+        transform: translateY(40px);
+    }
+    50% {
+        opacity: 0.5;
+    }
+    100% {
+        opacity: 1;
+        transform: translateY(0px);
+    }
+}
+
+:root {
+    --lightgreen: #80FFDB;
+    --cyan: #64DFDF;
+    --dark: #252525;
+    --white: #FFFFFF;
+    --purple: #6930C3;
+    --black: #000000;
+    --gray: #646262;
+    --yellow: #ffc229;
+    --pink: #ff78c4;
+    --green: #6CEE73;
+    --cyan-transparent: rgba(100, 223, 223, 0.8);
+    --black-transparent: rgba(0, 0, 0, 0.8);
+    --dark-transparent: rgba(15, 15, 16, 0.8);
+    --purple-transparent: rgba(105, 48, 195, 0.8);
+    --white-transparent: rgba(255, 255, 255, 0.8);
+}
+
+html {
+    scroll-behavior: smooth;
+}
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Poppins";
+}
+
+.tickets-card {
+    position: relative;
+    width: 350px;
+    overflow: hidden;
+}
+
+.tickets-card::after,
+.tickets-card::before {
+    content: "";
+    position: absolute;
+    top: calc(85% - 60px);
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+    background-color: var(--white);
+    z-index: 2;
+}
+
+.tickets-card::before {
+    left: -40px;
+}
+.tickets-card::after {
+    right: -40px;
+}
+
+.tickets-header {
+    width: 100%;
+    background-image: url("./assets/images/tickets.png");
+    background-position: center;
+    background-size: cover;
+    position: relative;
+    z-index: 1;
+}
+
+.tickets-header::before {
+    content: "";
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    top: 0;
+    background-color: rgba(0,0,0,0.5);
+}
+
+.tickets-content {
+    position: relative;
+    z-index: 2;
+    text-align: center;
+    padding: 10px;
+    color: var(--white);
+}
+
+.tickets-title {
+    padding-top: 20px;
+    margin-bottom: 20px;
+}
+
+.tickets-title p {
+    font-size: 24px;
+    font-family: "Poppins-SemiBold";
+}
+.tickets-title p:nth-child(2) {
+    font-size: 14px;
+    font-family: "Poppins";
+}
+
+.tickets-date {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 10px;
+    gap: 5px;
+}
+
+.tickets-date p:nth-child(2) {
+    font-size: 24px;
+    font-family: "Poppins-Bold";
+}
+
+.tickets-logo {
+    margin: 30px 0px;
+}
+
+.tickets-logo img {
+    width: 300px;
+}
+
+.tickets-type {
+    margin-bottom: 30px;
+}
+
+.tickets-type p {
+    font-size: 60px;
+    color: var(--yellow);
+    text-shadow: 0px 0px 10px var(--yellow);
+    font-family: "Poppins-Bold";
+}
+
+.tickets-price {
+    display: flex;
+    align-items: center;
+    gap: 5px;
+    color: var(--pink);
+    justify-content: center;
+}
+
+.tickets-price span {
+    position: relative;
+    top: 6px;
+    font-size: 24px;
+    font-family: "Poppins-Bold";
+}
+
+.tickets-price p {
+    font-size: 40px;
+    font-family: "Poppins-Bold";
+}
+
+.tickets-body {
+    width: 100%;
+    height: 100px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
+    background-color: var(--purple);
+    color: var(--white);
+}
+
+    </style>
+</head>
+<body>
+    <script>window.print()</script>
+        <div class="" style="display: flex; justify-content: center; margin-bottom: 20px;">
+        <div class="tickets-card">
+                        <div class="tickets-header">
+                            <div class="tickets-content">
+                                <div class="tickets-title">
+                                    <p>KAMPUNG JACKLOTH</p>
+                                    <p>Slamet Riyadi street, Jember</p>
+                                </div>
+                                
+                                <hr>
+                                
+                                <div class="tickets-date">
+                                    <p>August 31, 2024</p>
+                                    <p>9 PM</p>
+                                </div>
+
+                                <div class="tickets-logo">
+                                    <img src="./assets/images/logo.png" alt="">
+                                </div>
+
+                                <div class="tickets-type">
+                                    <p>GENERAL</p>
+                                </div>
+
+                                <div class="tickets-price">
+                                    <span>IDR</span>
+                                    <p>100.000</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="tickets-body">
+                            <p style="font-weight: 600;">Ticket ID</p>
+                            <p><?= $id_ticket ?></p>
+                        </div>
+                    </div>
+        </div>
+
+    <div class="" style="border-top: 2px dotted black;"></div>
+
+    <p>Hi Khairunnisa, here are some important information you need to know: <br>
+
+Date & Time : Saturday, August 31st | 9 PM<br>
+Location        : Kampung Jackloth, Slamet Riyadi street, Jember<br>
+Ticket             : Make sure you bring your physical ticket or e-ticket.<br>
+<br>
+Guidelines:<br>
+Please bring your KTP or other valid ID for verification.<br>
+No prohibited items such as weapons, alcohol, or illegal drugs are allowed.<br>
+Enjoy the concert while respecting other visitors.<br>
+<br>
+Services & Facilities:<br>
+Food court and merchandise are available in the concert area.<br>
+Parking area is in the backyard.<br>
+<br>
+Doors will open at 7 PM. We recommend arriving early to avoid long queues.<br>
+<br>
+See you at the concert, and let's create unforgettable memories together!<br>
+Warm Regards, Concert Organizer Team.</p>
+</body>
+</html>
